f54cbe6b83c09b506b7f556feff8a9fd1a332234
Robert Garner <rgarner011235@gmail.com>
2025-03-03 14:19:48 -0700

    Add project files.

---
+++ ContactApp.Client/Program.cs
@@ -0,0 +1,19 @@
+using ContactApp.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace ContactApp.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+        await builder.Build().RunAsync();
+    }
+}
